ac05841a8a61852f04b5a69436c94f60f57dcaee
Anton Perevoshchikov <anper3.5@gmail.com>
2019-11-07 11:54:11 +0300

    Adds namespace to function

--- src/functions.php
+++ src/functions.php
@@ -1,9 +1,11 @@
 <?php
 
+namespace Anper\PdoCollector;
+
 use Anper\PdoCollector\Statement;
 
 /**
- * @param PDO $pdo
+ * @param \PDO $pdo
  * @param callable $collector
  */
 function register_pdo_collector(\PDO $pdo, callable $collector)
